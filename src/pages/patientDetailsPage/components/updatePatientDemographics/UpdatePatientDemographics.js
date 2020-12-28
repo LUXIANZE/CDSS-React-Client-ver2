178c3691526544f63c49beb2f5c63097f7a28320
LUXIANZE <luxianze98@gmail.com>
2020-12-28 09:00:04 +0800

    Added state hook for user update input

--- src/pages/patientDetailsPage/components/updatePatientDemographics/UpdatePatientDemographics.js
+++ src/pages/patientDetailsPage/components/updatePatientDemographics/UpdatePatientDemographics.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
@@ -7,8 +7,39 @@ import DialogTitle from "@material-ui/core/DialogTitle";
 import TextField from "@material-ui/core/TextField";
 import { Button } from "@material-ui/core";
 
+import { AppContext } from "../../../../context";
+
 const UpdatePatientDemographics = (props) => {
+  const context = useContext(AppContext);
+  const [inputMRNNumber, setInputMRNNumber] = useState(
+    context.selectedPatient.mRNNumber ? context.selectedPatient.mRNNumber : ""
+  );
+  const [inputDateOfBirth, setInputDateOfBirth] = useState(
+    context.selectedPatient.dateOfBirth
+      ? context.selectedPatient.dateOfBirth
+      : ""
+  );
+  const [inputGender, setInputGender] = useState(
+    context.selectedPatient.gender ? context.selectedPatient.gender : ""
+  );
+  const [inputRace, setInputRace] = useState(
+    context.selectedPatient.race ? context.selectedPatient.race : ""
+  );
+  const [inputBMI, setInputBMI] = useState(
+    context.selectedPatient.bMI ? context.selectedPatient.bMI : ""
+  );
   const { open, handleClose } = props;
+
+  const handleSave = () => {
+    const updatedPatient = { ...context.selectedPatient };
+    updatedPatient.mRNNumber = inputMRNNumber;
+    updatedPatient.gender = inputGender;
+    updatedPatient.dateOfBirth = inputDateOfBirth;
+    updatedPatient.race = inputRace;
+    updatedPatient.bMI = inputBMI;
+    context.selectPatient(updatedPatient);
+    handleClose();
+  };
   return (
     <Dialog
       open={open}
@@ -22,19 +53,52 @@ const UpdatePatientDemographics = (props) => {
           autoFocus
           margin="dense"
           label="MRNNumber"
-          type="email"
           fullWidth
+          value={inputMRNNumber}
+          onChange={(event) => {
+            setInputMRNNumber(event.target.value);
+          }}
         />
-        <TextField autoFocus margin="dense" label="Date of Birth" fullWidth />
         <TextField
           autoFocus
           margin="dense"
-          label="MRNNumber"
-          type="Gender"
+          label="Date of Birth"
           fullWidth
+          value={inputDateOfBirth}
+          onChange={(event) => {
+            setInputDateOfBirth(event.target.value);
+          }}
         />
-        <TextField autoFocus margin="dense" label="Race" fullWidth />
-        <TextField autoFocus margin="dense" label="BMI" fullWidth />
+        <TextField
+          autoFocus
+          margin="dense"
+          label="Gender"
+          fullWidth
+          value={inputGender}
+          onChange={(event) => {
+            setInputGender(event.target.value);
+          }}
+        />
+        <TextField
+          autoFocus
+          margin="dense"
+          label="Race"
+          fullWidth
+          value={inputRace}
+          onChange={(event) => {
+            setInputRace(event.target.value);
+          }}
+        />
+        <TextField
+          autoFocus
+          margin="dense"
+          label="BMI"
+          fullWidth
+          value={inputBMI}
+          onChange={(event) => {
+            setInputBMI(event.target.value);
+          }}
+        />
       </DialogContent>
       <DialogActions>
         <Button
@@ -45,11 +109,11 @@ const UpdatePatientDemographics = (props) => {
           Cancel
         </Button>
         <Button
-          onClick={handleClose}
+          onClick={handleSave}
           color="primary"
           style={{ backgroundColor: "#25C8C8", color: "white" }}
         >
-          Subscribe
+          Save
         </Button>
       </DialogActions>
     </Dialog>
