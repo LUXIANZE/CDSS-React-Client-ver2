3e8e5fd4d9c8e8ab24dbfa7c47828bd73e3b511e
LUXIANZE <luxianze98@gmail.com>
2020-12-28 09:31:50 +0800

    Minor Dashboard css adjustment

--- src/pages/dashboardPage/DashboardPage.js
+++ src/pages/dashboardPage/DashboardPage.js
@@ -1,25 +1,29 @@
 import React from "react";
 import { makeStyles } from "@material-ui/styles";
 import {
+  BarChart,
+  Bar,
+  CartesianGrid,
+  Legend,
   LineChart,
   Line,
-  CartesianGrid,
   XAxis,
   YAxis,
   Tooltip,
   PieChart,
   Pie,
-  Sector,
   Cell,
 } from "recharts";
+import Typography from "@material-ui/core/Typography";
 
 import Layout from "../../layout";
 
 const useStyles = makeStyles({
   container: {
     display: "flex",
-    flexDirection: "row",
+    flexDirection: "column",
     flexGrow: 1,
+    overflow: "scroll",
   },
 });
 
@@ -68,13 +72,24 @@ const data = [
   { name: "Saturday", uv: 400, pv: 2400, amt: 2400 },
 ];
 
+const data3 = [
+  { name: "Sunday", Accepted: 4000, Rejected: 2400, amt: 2400 },
+  { name: "Monday", Accepted: 3000, Rejected: 1398, amt: 2210 },
+  { name: "Tuesday", Accepted: 2000, Rejected: 9800, amt: 2290 },
+  { name: "Wednesday", Accepted: 2780, Rejected: 3908, amt: 2000 },
+  { name: "Thursday", Accepted: 1890, Rejected: 4800, amt: 2181 },
+  { name: "Friday", Accepted: 2390, Rejected: 3800, amt: 2500 },
+  { name: "Saturday", Accepted: 3490, Rejected: 4300, amt: 2100 },
+];
+
 const DashboardPage = () => {
   const classes = useStyles();
 
   return (
     <Layout title="Dashboard Page">
       <div className={classes.container}>
-        <div style={{ margin: 50 }}>
+        <div style={{ margin: 50, alignSelf: "center" }}>
+          <Typography variant="h4">Weekly reports count</Typography>
           <LineChart
             width={800}
             height={400}
@@ -88,7 +103,8 @@ const DashboardPage = () => {
             <Tooltip />
           </LineChart>
         </div>
-        <div style={{ margin: 50 }}>
+        <div style={{ margin: 50, alignSelf: "center" }}>
+          <Typography variant="h4">Alltime reports categories</Typography>
           <PieChart width={800} height={400}>
             <Pie
               data={data1}
@@ -104,6 +120,24 @@ const DashboardPage = () => {
               ))}
             </Pie>
           </PieChart>
+        </div>
+
+        <div style={{ margin: 50, alignSelf: "center" }}>
+          <Typography variant="h4">Decision accepted and rejected</Typography>
+          <BarChart
+            width={800}
+            height={400}
+            data={data3}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="Rejected" fill="#8884d8" />
+            <Bar dataKey="Accepted" fill="#82ca9d" />
+          </BarChart>
         </div>
       </div>
     </Layout>
